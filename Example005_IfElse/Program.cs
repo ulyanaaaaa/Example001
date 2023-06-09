0c4ea50cc73fffa94c37b498809646028a784f27
Ulyana <ulyanaharbuz@gmail.com>
2023-06-09 20:10:26 +0200

    Example 005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Print username ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ulyana")
+{
+    Console.WriteLine("Nice to meet you!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
